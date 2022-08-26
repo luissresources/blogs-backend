e7b0d6a4d6250d80ae2026de759e074c20f3699c
Luiss Resources <luissresources@gmail.com>
2022-08-25 23:51:02 -0500

    feat: update app.js with configuration for cypress

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
+if(process.env.NODE_ENV === 'test') {
+  const testRouter = require('./controllers/test')
+  app.use('/api/testing', testRouter)
+}
+
 app.use(middleware.errorHandler)
 
 module.exports = app
